f17f88822a305d4b05fa0ee340c67c6ef132ae3e
CatarinaVRGA <catarinavitoriarga@gmail.com>
2026-09-09 11:34:50 -0300

    novo codigo alterado

--- app/DataBase.php
+++ app/DataBase.php
@@ -7,31 +7,25 @@ use PDOException;
 
 class DataBase
 {
-
     private const HOST = 'localhost';
-
     private const USER = 'root';
-
     private const PASSWORD = '123';
-
-    private const DBNAME = 'supermercado';
-
+    private const DB = 'supermercado';
     private $connection;
-
     private $table;
-    //metodo que constroi classe
+    //metodo que constroi a classe
     public function __construct($table = null)
     {
         $this->table = $table;
         $this->setConnection();
     }
-    //metodo que cria uma conexão com o banco dedados
+    //metodo que cria uma conexão com o banco de dados
     public function setConnection()
     {
-        $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::DBNAME, self::USER, self::PASSWORD);
+        $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::DB, self::USER, self::PASSWORD);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-
+    //metodo que insere dados no banco
     public function execute($query, $values = null)
     {
         try {
@@ -45,8 +39,6 @@ class DataBase
             die('ERRO: ' . $e);
         }
     }
-
-    //metodo que insere dados no banco
     public function insert($array)
     {
         //extrair as chaves do array
@@ -54,55 +46,40 @@ class DataBase
         //criar um array com valores = ?
         $binds = array_pad([], count($array), '?');
         //monta a query
-        $query = 'INSERT INTO ' . $this->table . ' (' . implode(', ', $fields) . ') VALUES(' . implode(', ', $binds) . ')';
+        $query = 'INSERT INTO ' . $this->table . ' (' . implode(',', $fields) . ') VALUES (' . implode(',', $binds) . ')';
         //executa a query
         $this->execute($query, array_values($array));
         return $this->connection->lastInsertId();
     }
-
-    public function update($where, $array){
-         //extrair as chaves do array
+    public function update($where, $array)
+    {
+        //extrair as chaves do array
         $fields = array_keys($array);
         //monta a query
-        $query = 'UPDATE ' . $this->table . ' SET ' . implode('=?, ', $fields) . '=? WHERE ' . $where;
+        $query = 'UPDATE ' . $this->table . ' SET ' . implode('=?,', $fields) . ' WHERE ' . $where;
         //executa a query
         $this->execute($query, array_values($array));
         return true;
     }
-
     public function delete($where)
     {
+        //monta a query
         $query = 'DELETE FROM ' . $this->table . ' WHERE ' . $where;
         $this->execute($query);
         return true;
     }
-
     public function select($where = null, $order = null, $limit = null, $fields = '*')
     {
-        $where = is_string($where) && $where !== '' ? ' where ' . $where : '';
-        $order = is_string($order) && $order !== '' ? ' order by ' . $order : '';
-        $limit = is_string($limit) && $limit !== '' ? ' limit ' . $limit : '';
-        $query = 'select ' . $fields . ' from ' . $this->table . $where . $order . $limit;
+        $where = strlen($where) ? 'ORDER BY ' . $where : '';
+        $order = strlen($order) ? 'ORDER BY ' . $order : '';
+        $limit = strlen($limit) ? 'LIMIT ' . $limit : '';
+        $query = 'SELECT ' . $fields . ' FROM ' . $this->table . ' ' . $where . ' ' . $order . ' ' . $limit;
         return $this->execute($query);
     }
-
-
-
 }
-
 $db = new DataBase('fornecedor');
-$db->delete('nome = "Coca-Cola"');
-$f = $db->select()->fetchAll(PDO::FETCH_ASSOC);
-$db->insert([
-    'nome' => 'Coca-Cola',
-    'cnpj' => '12345678444',
-    'telefone' => '5427655',
-    'email' => 'cocacola@email.com',
-    'endereco' => 'Avenida das Américas, 456'
-]);
-$db->select('nome = "Coca-Cola"');
-$f = $db->select()->fetchAll(PDO::FETCH_ASSOC);
+$db->delete('id=1');
+$f = $db->select(null, 'cnpj desc', 2, 'nome, cnpj')->fetchAll(PDO::FETCH_CLASS);
 echo "<pre>";
 print_r($f);
 echo "</pre>";
-
