50644f67cea918ad0c55c0e4539a90ec32e17c1b
CatarinaVRGA <catarinavitoriarga@gmail.com>
2026-09-09 11:29:51 -0300

    novo codigo

--- app/DataBase.php
+++ app/DataBase.php
@@ -23,45 +23,86 @@ class DataBase
     public function __construct($table = null)
     {
         $this->table = $table;
+        $this->setConnection();
     }
     //metodo que cria uma conexão com o banco dedados
     public function setConnection()
     {
-        $connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::DBNAME, self::USER, self::PASSWORD);
+        $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::DBNAME, self::USER, self::PASSWORD);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public function execute($query, $values){
-        try{
+    public function execute($query, $values = null)
+    {
+        try {
             echo "<pre>";
             print_r($query);
             echo "</pre>";
-        }catch(PDOException $e){
-            die('ERRO: '.$e);
+            $statement = $this->connection->prepare($query);
+            $statement->execute($values);
+            return $statement;
+        } catch (PDOException $e) {
+            die('ERRO: ' . $e);
         }
-
-      
     }
 
     //metodo que insere dados no banco
-    public function insert($array){
+    public function insert($array)
+    {
         //extrair as chaves do array
         $fields = array_keys($array);
         //criar um array com valores = ?
         $binds = array_pad([], count($array), '?');
         //monta a query
-        $query = 'INSERT INTO ' .$this->table.' (' . implode(', ', $fields) . ') 
-        VALUES(' . implode(', ', $binds) . ')';
+        $query = 'INSERT INTO ' . $this->table . ' (' . implode(', ', $fields) . ') VALUES(' . implode(', ', $binds) . ')';
         //executa a query
         $this->execute($query, array_values($array));
-        return $this->connection->lasInsertId();
-       
+        return $this->connection->lastInsertId();
     }
+
+    public function update($where, $array){
+         //extrair as chaves do array
+        $fields = array_keys($array);
+        //monta a query
+        $query = 'UPDATE ' . $this->table . ' SET ' . implode('=?, ', $fields) . '=? WHERE ' . $where;
+        //executa a query
+        $this->execute($query, array_values($array));
+        return true;
+    }
+
+    public function delete($where)
+    {
+        $query = 'DELETE FROM ' . $this->table . ' WHERE ' . $where;
+        $this->execute($query);
+        return true;
+    }
+
+    public function select($where = null, $order = null, $limit = null, $fields = '*')
+    {
+        $where = is_string($where) && $where !== '' ? ' where ' . $where : '';
+        $order = is_string($order) && $order !== '' ? ' order by ' . $order : '';
+        $limit = is_string($limit) && $limit !== '' ? ' limit ' . $limit : '';
+        $query = 'select ' . $fields . ' from ' . $this->table . $where . $order . $limit;
+        return $this->execute($query);
+    }
+
+
+
 }
+
 $db = new DataBase('fornecedor');
+$db->delete('nome = "Coca-Cola"');
+$f = $db->select()->fetchAll(PDO::FETCH_ASSOC);
 $db->insert([
     'nome' => 'Coca-Cola',
-    'cnpj' => 'ABC123456789',
+    'cnpj' => '12345678444',
     'telefone' => '5427655',
     'email' => 'cocacola@email.com',
     'endereco' => 'Avenida das Américas, 456'
 ]);
+$db->select('nome = "Coca-Cola"');
+$f = $db->select()->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($f);
+echo "</pre>";
+
---
+++ commands.md
@@ -0,0 +1,17 @@
+docker run --rm -v ".:/app" composer update
+
+docker compose up -d
+
+Comandos GIT
+
+adicionar um arquivo git add
+
+adicionar todas as modificações git add .
+
+realizar o commit git commit -m "<descrição do commit>"
+
+realizar o push
+
+git push origin
+
+realizar o push no main git push origi main
